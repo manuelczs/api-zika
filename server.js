7d1d3d0bc2713b8a609391a46d3042fa0e784e6e
manuelczs <manuelczs@outlook.com>
2018-11-04 16:54:58 -0300

    first-commit

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var URL_CSV = 'http://datos.salud.gob.ar/dataset/ceaa8e87-297e-4348-84b8-5c643e172500/resource/318834df-656d-4e71-9e87-195ebd96a0f8/download/informacion-publica-dengue-zika-nacional-hasta-20180821v2.csv';
+var request = require('request');
+var csv = require('csvtojson');
+var http = require('http')
+var fs = require('fs');
+var path = require('path');
+var csvjson = require('csvjson');
+
+
+var request = http.get(URL_CSV, function(response) {
+    if (response.statusCode === 200) {
+        var file = fs.createWriteStream("./csv/file.csv");
+        response.pipe(file);
+    }
+    // Add timeout.
+    request.setTimeout(12000, function () {
+        request.abort();
+    });
+});
+
+var options = {
+	delimiter: ',',
+	quote: '"'
+};
+
+var file_data = fs.readFileSync('./csv/file.csv', { encoding: 'utf8' });
+var json_result = csvjson.toObject(file_data, options);
+var data = JSON.stringify(json_result);
+fs.writeFileSync('./json/final-json.json', data);
+
+
+app.get('/', (req, res) => console.log(data));
+app.listen(3000, (req, res) => console.log('port 3000 listening...'));
