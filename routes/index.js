f23801e0c75db544b72aa41ace3997629f227350
manuel <manuelczs@outlook.com>
2021-03-29 02:44:41 -0300

    refactoring routes/index.js file to use ES6 syntax and import services changed name

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,40 @@
-var express = require('express');
+import express from 'express';
+import fs from 'fs';
+import services from '../services';
+
 var routes = express();
-var fs = require('fs');
 var router = express.Router();
 var jsonData = JSON.parse(fs.readFileSync('./json/final-json.in', 'utf8'));
-var codes = require('../services');
 
 // set the Router Middleware
 routes.use(router);
 
 routes.get('/api/provinces', (req, res) => {
   if (jsonData) {
-    res.status(200).json(codes.getProvinces(jsonData));
+    res.status(200).json(services.getProvinces(jsonData));
   } else {
     res.send('Nothing here');
   }
 });
 
 routes.get('/api/departaments', (req, res) => {
-  res.status(200).json(codes.getDeparts(jsonData));
+  res.status(200).json(services.getDeparts(jsonData));
 });
 
 routes.get('/api/deps_by_prov/:prov', (req, res) => {
-  res.status(200).json(codes.getDepsByProv(`${req.params.prov}`, jsonData));
+  res.status(200).json(services.getDepsByProv(`${req.params.prov}`, jsonData));
 });
 
 routes.get('/api/total_dengue', (req, res) => {
-  res.status(200).json(codes.getTotalDengue(jsonData));
+  res.status(200).json(services.getTotalDengue(jsonData));
 });
 
 routes.get('/api/total_zika', (req, res) => {
-  res.status(200).json(codes.getTotalZika(jsonData));
+  res.status(200).json(services.getTotalZika(jsonData));
 });
 
 routes.get('/api/departaments', (req, res) => {
-  res.status(200).json(codes.getDeparts(jsonData));
+  res.status(200).json(services.getDeparts(jsonData));
 });
 
 module.exports = routes;
