bb27bc6d7cd8d161fd1e9d9c84518d7ecff2063f
manuel <manuelczs@outlook.com>
2021-03-30 02:38:24 -0300

    axios library implemented in /map route, it call to google Api

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 import express from 'express';
 import morgan from 'morgan';
 import csv from 'csvtojson';
+import axios from 'axios';
 import http from 'http';
 import path from 'path';
 import csvjson from 'csvjson';
-import bodyParser from 'body-parser';
 import api from './routes';
 
-var app = express();
-var port = 3000;
+const app = express();
+const port = 3000;
+const navigation = [
+  { link: '/', name: 'Casos' },
+  { link: '/map', name: 'Mapa' },
+  { link: '/contact', name: 'Contacto' },
+];
+const API_KEY = 'AIzaSyAUYDtv51kgaUx_wEw3bfWkoSiYrXuKGlA';
 //var jsonData = JSON.parse(fs.readFileSync('./json/final-json.in', 'utf8'));
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 // ******************************************************** //
@@ -43,7 +48,56 @@ fs.writeFileSync('./json/final-json.in', data);
 
 app.use(api);
 app.get('/', (req, res) => {
-  res.render('index', { text: 'text-1', text1: 'text-2' });
+  res.render('index', { text: 'text-1', text1: 'text-2', navigation });
+});
+
+app.get('/map', (req, res) => {
+  let provinces = [];
+  let provs_coords = [];
+  let provs_country = [];
+
+  axios
+    .get('http://localhost:3000/api/provinces')
+    .then((response) => {
+      provinces = response.data.provinces;
+
+      provs_country = provinces.map((prov) => prov + ', Argentina');
+      //for (let i = 0; i < provinces.length; i++) {
+      //  provs_country.push(provinces[i] + ', Argentina');
+      //}
+
+      provs_country.map((prov) => {
+        axios
+          .get(
+            `ttps://maps.googleapis.com/maps/api/geocode/json?address=${prov}&key=${API_KEY}`
+          )
+          .then((response) => {
+            provs_coords.push(response.data.results[0].geometry.location);
+            //console.log(response.data.results[0].geometry.location);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      });
+
+      //for (let i = 0; i < provinces.length; i++) {
+      //  let LOCATION = provs_country[i];
+      //  let url_api = `https://maps.googleapis.com/maps/api/geocode/json?address=${LOCATION}&key=${API_KEY}`;
+      // Change this above line
+      //let position = $.getJSON(url_api).responseJSON.results[0].geometry.location;
+      //  provs_coords.push(position);
+      // }
+    })
+    .then(() => {
+      res.render('map', { navigation, provinces, provs_coords });
+    })
+    .catch((err) => {
+      console.log('There was an error: ' + err);
+    });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { navigation });
 });
 
 app.listen(port, () => console.log(`port ${port} listening...`));
