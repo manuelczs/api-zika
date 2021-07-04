bb2c577e84b869f00c57508fe34574c6ee97f397
Manuel <manuelczs@outlook.com>
2021-07-03 22:33:21 -0300

    email, emailService and emailPassword added

--- config/index.js
+++ config/index.js
@@ -3,7 +3,10 @@ dotenv.config();
 
 const config = {
   port: process.env.PORT,
-  apiKeyGoogle: process.env.API_KEY
+  apiKeyGoogle: process.env.API_KEY,
+  email: process.env.EMAIL,
+  emailService: process.env.EMAIL_SERVICE,
+  emailPassword: process.env.EMAIL_PASSWORD,
 }
 
 module.exports = config;
